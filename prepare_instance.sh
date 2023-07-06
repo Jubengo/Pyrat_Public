b59b6ab2744ce25d07e2e201e271aedea242e61f
Jubengo <138203013+Jubengo@users.noreply.github.com>
2023-07-06 18:17:01 +0200

    Update prepare_instance.sh

--- prepare_instance.sh
+++ prepare_instance.sh
@@ -8,13 +8,17 @@ echo $4
 PyRAT=/home/ubuntu/toolkit
 
 CATEGORY=$2
-ONNX_FILE=$3
-VNNLIB_FILE=$4
-source "${HOME}/conda/etc/profile.d/conda.sh"
-source "${HOME}/conda/etc/profile.d/mamba.sh"
-conda activate
-source ~/.bashrc
-mamba activate temp_pyrat
+if [[$CATEGORY = "vit" || $CATEGORY = "carvana_unet_2022" || $CATEGORY = "cctsdb_yolo" || $CATEGORY = "ml4acopf"]]
+then
+  return -1
+else
+  ONNX_FILE=$3
+  VNNLIB_FILE=$4
+  source "${HOME}/conda/etc/profile.d/conda.sh"
+  source "${HOME}/conda/etc/profile.d/mamba.sh"
+  conda activate
+  source ~/.bashrc
+  mamba activate temp_pyrat
 
-python $PyRAT/pyrat.pyc --read "parsing_only" --model_path $ONNX_FILE --property_path $VNNLIB_FILE \
---config $PyRAT/vnn_config/$CATEGORY.ini
+  python $PyRAT/pyrat.pyc --read "parsing_only" --model_path $ONNX_FILE --property_path $VNNLIB_FILE \
+  --config $PyRAT/vnn_config/$CATEGORY.ini
